f574ef22cd45a8be822aa8274fa14a51f7bd73a8
CFL 401 <CFL 401@DESKTOP-R1BJ0LR>
2019-06-28 19:18:40 -0300

    Inicio de proyecto

---
+++ Goncalves - Proyecto Lunes/src/goncalves/proyecto/lunes/Lunes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goncalves.proyecto.lunes;
+
+/**
+ *
+ * @author CFL 401
+ */
+public class Lunes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
